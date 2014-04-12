5066d62842f5bd989eeb4c1a8e28cc3762f680d1
Stanislav Mekhonoshin <ejabberd@gmail.com>
2014-04-12 15:52:54 +0400

    Refactor

--- src/clj_test_web/parser.clj
+++ src/clj_test_web/parser.clj
@@ -1,30 +1,46 @@
 (ns clj-test-web.parser)
 (require '[clojure.string :as str])
 
-(defn params_string [query]
+(defn params_string
+  [query]
   (if
     (> (count (str/split query #"\?")) 1)
     (nth (str/split query #"\?") 1)
     ""))
 
-(defn url_string [query]
+(defn url_string
+  [query]
   (nth (str/split query #"\?") 0))
 
-(defn url_with_params [request]
+(defn url_with_params
+  [request]
   (nth (str/split request #" ") 1))
 
 (defn params_vector
   [request]
-  (str/split (params_string (url_with_params request)) #"(&|=)"))
+  (str/split
+    (params_string
+      (url_with_params request))
+    #"(&|=)"))
 
-(defn params [request]
+(defn params_present?
+  [request]
+  (> (count (params_vector request)) 1))
+
+(defn params
+  [request]
   (if
-    (> (count (params_vector request)) 1)
+    (params_present? request)
     (apply hash-map (params_vector request)))
     [])
 
-(defn path [request]
-  (str/replace-first (url_string (url_with_params request)) #"^.*?/" ""))
+(defn path
+  [request]
+  (str/replace-first
+    (url_string
+      (url_with_params request))
+    #"^.*?/" ""))
 
-(defn parse [request]
+(defn parse
+  [request]
   { :path (path request) :params (params request) })
